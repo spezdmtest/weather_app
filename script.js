8e2a0b26a071d495c6047f338a19ec46c18bb151
spezdmtest <spezd.m.@gmail.com>
2024-03-16 15:53:09 +0200

    add store

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const link =
   "http://api.weatherstack.com/current?access_key=fc3a599323c964c600f1a162cc442b4a";
 
+const store = {
+  city: "Romny",
+
+}  
+
 const fetchData = async () => {
-  const result = await fetch(`${link}&query=Romny`);
+  const result = await fetch(`${link}&query=${store.city}`);
   const data = await result.json();
   console.log(data);
 };
